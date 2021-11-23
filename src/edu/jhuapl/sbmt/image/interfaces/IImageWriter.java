e7c9cd443a484af47b27d5cdfc73f4b3adac2007
Josh Steele <josh.steele@jhuapl.edu>
2021-11-23 17:58:58 -0500

    RM 2356: reorganized, made new image tab for testing

--- src/edu/jhuapl/sbmt/image/interfaces/IImageWriter.java
+++
@@ -1,6 +0,0 @@
-package edu.jhuapl.sbmt.image.interfaces;
-
-public interface IImageWriter
-{
-
-}
--- src/edu/jhuapl/sbmt/image/pipeline/IPipelineComponent.java
+++
@@ -1,8 +0,0 @@
-package edu.jhuapl.sbmt.image.pipeline;
-
-import java.io.IOException;
-
-public interface IPipelineComponent
-{
-	public void run() throws IOException, Exception;
-}
